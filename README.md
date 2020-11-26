08d3e0c866ff426ccfc2496fa66a144bcc6445b9
Maxime Baumann <maxime.baumann.pro@gmail.com>
2020-11-26 19:52:25 +0100

    Fin révisions + début package manager

--- README.md
+++ README.md
@@ -10,5 +10,15 @@ Conseils pratiques
 
 Créez-vous un compte sur [GitHub][1] puis demandez un accès à [GitHub Education][2] pour bénéficier de différents avantages.
 
+Utilisation du dépôt
+--------------------
+
+- [NodeJS][3]
+- [Parcel][4]
+- `npm ci` Pour installer les dépendances
+- `parcel forms.html`
+
 [1]: https://github.com
 [2]: https://education.github.com/students
+[3]: https://nodejs.org/en/
+[4]: https://parceljs.org/getting_started.html
--- javascript/forms.jquery.js
+++ javascript/forms.jquery.js
@@ -1,3 +1,5 @@
+import $ from 'jquery';
+
 $(function () {
     $('.no-js').removeClass('no-js');
 
@@ -7,6 +9,52 @@ $(function () {
     const $step2 = $('#step2');
     const $abortpart2 = $('#abortpart2')
     const $prev = $('#prev');
+    const $register = $('#register')
+
+    function doesMandatoryFieldsHaveErrors() {
+        let hasErrors = false;
+
+        $step1.find('.hint').hide();
+
+        $step1.find('input').each(function () {
+            // Si erreur -> hasErrors = true
+            const $this = $(this);
+
+            if ($this.attr('data-minlength') && $this.val().length < $this.attr('data-minlength')) {
+                hasErrors = true;
+                $this.next('[data-hint=minlength]').show();
+            }
+
+            if ($this.attr('data-equalsto') && $this.val() !== $('#' + $this.attr('data-equalsto')).val()) {
+                hasErrors = true;
+                $this.nextAll('[data-hint=equalsto]').show();
+            }
+
+            if ($this.attr('data-maxyear') && $this.val() && $this.val().split('-')[0] > $this.attr('data-maxyear')) {
+                hasErrors = true;
+                $this.nextAll('[data-hint=maxyear]').show();
+            }
+        })
+
+        return hasErrors;
+    }
+
+    function goOnFirstForm() {
+        $step2.hide();
+        $step1.show();
+    }
+
+    $register.on('submit', function (e) {
+        e.preventDefault();
+
+        if (!doesMandatoryFieldsHaveErrors()) {
+            alert('Bienvenue sur notre site !');
+        } else {
+            goOnFirstForm();
+        }
+
+        return false;
+    })
 
     $abort.on('click', function(e) {
         e.preventDefault();
@@ -26,21 +74,7 @@ $(function () {
     $next.on('click', function (e) {
         e.preventDefault();
 
-        let hasErrors = false;
-
-        $step1.find('.hint').hide();
-
-        $step1.find('input').each(function () {
-            // Si erreur -> hasErrors = true
-            const $this = $(this);
-
-            if ($this.attr('data-minlength') && $this.val().length < $this.attr('data-minlength')) {
-                hasErrors = true;
-                $this.next('[data-hint=minlength]').show();
-            }
-        })
-
-        if (!hasErrors) {
+        if (!doesMandatoryFieldsHaveErrors()) {
             $step1.hide();
             $step2.show();
         }
@@ -50,8 +84,7 @@ $(function () {
     $prev.on('click', function (e) {
         e.preventDefault();
 
-        $step2.hide();
-        $step1.show();
+        goOnFirstForm();
 
         return false;
     })
